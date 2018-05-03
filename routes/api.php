5bff686023885d81ef6845fec84e4912130e7939
Risal Hidayat <risalhidayat88@gmail.com>
2018-05-03 16:09:06 +0700

    setup routes for user endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('users', 'Api\UserController@index');
+Route::post('users', 'Api\UserController@store');
